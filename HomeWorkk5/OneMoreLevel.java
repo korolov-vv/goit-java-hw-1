8d41c2c8c439d6716a6870768c1db4e157916d33
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-01-19 00:08:03 +0100

    add ex. 29

---
+++ HomeWorkk5/OneMoreLevel.java
@@ -0,0 +1,48 @@
+package HomeWorkk5;
+
+public class OneMoreLevel {
+    private int width;
+    private int height;
+
+    public OneMoreLevel(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
+
+class LevelTooBigException extends Exception{
+    LevelTooBigException(){
+        super("Level too big");
+    }
+}
+
+class OneMoreLevelLoader {
+    public void load(OneMoreLevel oneMoreLevel) throws LevelTooBigException {
+        if(oneMoreLevel.getWidth() * oneMoreLevel.getHeight() <= 100000) {
+            System.out.println("Level loaded");
+        } else {
+           throw new LevelTooBigException();}
+    }
+}
+
+class OneMoreLevelLoaderTest {
+    public static void main(String[] args) {
+        //Level loaded
+        new OneMoreLevelLoader().load(new OneMoreLevel(10, 20));
+
+        //Level too big
+        try {
+            new OneMoreLevelLoader().load(new OneMoreLevel(10000, 2000));
+        } catch (LevelTooBigException ex) {
+            System.out.println("Level too big");
+        }
+    }
+}
