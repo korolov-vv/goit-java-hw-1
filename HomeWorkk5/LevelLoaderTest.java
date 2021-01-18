34adbc3c4ee918ae61221ee853f7b1598b7518fd
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-01-18 12:51:54 +0100

    add ex 22

--- HomeWorkk5/LevelLoaderTest.java
+++ HomeWorkk5/LevelLoaderTest.java
@@ -10,6 +10,12 @@ class LevelLoaderTest {
     }
 }
 class LevelLoader{
+    private static LevelLoader instance = new LevelLoader();
+
+    public static LevelLoader getInstance() {
+        return instance;
+    }
+
     public String load(String levelName){
         return "Loading level " + levelName + " ...";
     }
