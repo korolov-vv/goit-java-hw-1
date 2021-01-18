162f307e9de04f12e4b61c8005512cc4d6b5dfad
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-01-18 23:27:35 +0100

    ad ex. 28

---
+++ HomeWorkk5/TotalScore.java
@@ -0,0 +1,22 @@
+package HomeWorkk5;
+
+class TotalScore {
+    public int sum(int[] scores) {
+        int result = 0;
+        int index = 0;
+
+        for (; index < scores.length ; index++) {
+            result += scores[index];
+        }
+        return result;
+    }
+}
+
+class TotalScoreTest {
+    public static void main(String[] args) {
+        int[] score = {10, 20, 100};
+
+        //130
+        System.out.println(new TotalScore().sum(score));
+    }
+}
