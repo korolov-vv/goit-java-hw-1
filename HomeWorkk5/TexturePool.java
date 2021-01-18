90e6393e2b85e28b359b483d5597ba827e287c59
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-01-18 13:47:12 +0100

    add ex. 24

---
+++ HomeWorkk5/TexturePool.java
@@ -0,0 +1,22 @@
+package HomeWorkk5;
+
+class TexturePool{
+    private static TexturePool instance = new TexturePool();
+
+    public static TexturePool getInstance() {
+        return instance;
+    }
+
+    private TexturePool() {
+    }
+
+    public String getTexture(String textureName){
+        return "Get texture " + textureName;
+    }
+}
+class TexturePoolTest {
+    public static void main(String[] args) {
+        //Get texture MainHero
+        System.out.println(TexturePool.getInstance().getTexture("MainHero"));
+    }
+}
