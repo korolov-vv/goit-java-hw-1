a2722c0b219539ecd654b9805ded260e4fcb6bf2
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-01-22 01:04:59 +0100

    Add ex. 3

---
+++ HomeWork6/GooSearchResult.java
@@ -0,0 +1,29 @@
+package HomeWork6;
+
+import java.util.Arrays;
+
+public class GooSearchResult {
+    private String url;
+
+    public GooSearchResult(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String parseDomain(){
+        String[] strArr = url.split("/");
+        return strArr[2];
+    }
+}
+class GooSearchResultTest {
+    public static void main(String[] args) {
+        //test.com
+        System.out.println(new GooSearchResult("https://test.com").parseDomain());
+
+        //apple.in.mars
+        System.out.println(new GooSearchResult("http://apple.in.mars").parseDomain());
+    }
+}
