bf387daf19a9d72874972b72003f674067d54f29
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-01-28 01:31:05 +0100

    add ex 25-30 HW 4

---
+++ HomeWork4/FirNum.java
@@ -0,0 +1,84 @@
+package HomeWork4;
+
+public class FirNum {
+    public int calc(int n){
+        return n;
+    }
+}
+class FirNumSum extends FirNum {
+    @Override
+    public int calc(int n){
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
+class FirNumFactorial extends FirNum {
+    @Override
+    public int calc(int n) {
+        int result = 1;
+        if(n == 0){
+            return result;
+        }else {
+            for (int i = 1; i <= n; i++) {
+                result *= i;
+            }
+        }
+        return result;
+    }
+}
+class FirNumMultiplyOdd extends FirNum {
+    @Override
+    public int calc(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            if(i % 2 != 0){
+                result *= i;
+            }
+        }
+        return result;
+    }
+}
+class FirNumFizzBuzz extends FirNum {
+    @Override
+    public int calc(int n) {
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            if(((i % 3 == 0) && !(i % 5 == 0)) | (!(i % 3 == 0) && (i % 5 == 0))){
+                result += i;
+            }
+        }
+        return result;
+    }
+}
+class FirTest {
+    public int test(FirNum firNum, int number) {
+        return firNum.calc(number);
+    }
+
+    public static void main(String[] args) {
+        FirNum firNum = new FirNum();
+        FirTest firTest = new FirTest();
+        FirNum firNum1 = new FirNumFactorial();
+        FirNum firNum2 = new FirNumSum();
+        FirNum firNum3 = new FirNumMultiplyOdd();
+        FirNum firNum4 = new FirNumFizzBuzz();
+
+        //Should be 10
+        System.out.println(firTest.test(firNum, 10));
+
+        //Should be 6
+        System.out.println(firTest.test(firNum2, 3));
+
+        //Should be 120
+        System.out.println(firTest.test(firNum1, 5));
+
+        //Should be 15
+        System.out.println(firTest.test(firNum3, 5));
+
+        //Should be 83
+        System.out.println(firTest.test(firNum4, 20));
+    }
+}
---
+++ HomeWork4/SpaceObject.java
@@ -0,0 +1,20 @@
+package HomeWork4;
+
+abstract class SpaceObject {
+}
+abstract class Planet extends SpaceObject {
+
+}
+class Mars extends Planet {
+
+}
+class Moon extends SpaceObject {
+
+}
+class PlanetTester {
+    String test(SpaceObject object) {
+        if(object instanceof Planet){
+            return "planet";
+        }else return "not planet";
+    }
+}
